e869fb43fccaa828a36f2a2d6d1b2f64ad2018de
Nilesh Narkhede <46423715+Nil15394@users.noreply.github.com>
2021-05-14 22:00:04 +0530

    Committing gitignore files

---
+++ SupportPortal/SupportPortalBackEnd/src/main/java/com/supportportal/SupportPortalBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.supportportal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupportPortalBackEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SupportPortalBackEndApplication.class, args);
+    }
+
+}
---
+++ SupportPortal/SupportPortalBackEnd/src/test/java/com/supportportal/supportportalbackend/SupportPortalBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.supportportal.supportportalbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupportPortalBackEndApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
